cbe5d7999bb90c2e67d9bab1cb9be2877e730953
Ebenezer Obasi <ebenzforcashmoney@gmail.com>
2023-12-27 00:10:11 +0000

    Update theme.sh
    Update THEME_STATIC_DIR="PATH-TO-MEUTIV/mt_static/themes/THEME_NAME" to reflect the directory of theme static files

--- theme.sh
+++ theme.sh
@@ -7,7 +7,7 @@ sass src/home.scss css/home.css --style=compressed
 sass src/admin.scss css/admin.css --style=compressed
 
 # Tweak for dynamic static
-THEME_STATIC_DIR="/Library/WebServer/Documents/public_html/websites/Webcilo/Public/webcilo.com/www/assets/themes/peepso"
+THEME_STATIC_DIR="PATH-TO-MEUTIV/mt_static/themes/THEME_NAME"
 
 # Watch Sass
 sass --watch src/default.scss:${THEME_STATIC_DIR}/css/default.css --style=compressed
